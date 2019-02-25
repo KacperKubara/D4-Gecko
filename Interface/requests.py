5b1592e172760dce974bdb67df6a23233e47ca2d
KacperKubara <kjk1u17@soton.ac.uk>
2019-02-25 10:48:21 +0000

    request.py updated

--- Interface/requests.py
+++ Interface/requests.py
@@ -1,6 +1,6 @@
 import requests
 import json
-
+import time 
 grip_data          = {'front_grip': 250, 'rear_grip': 100, 'bottom_grip': 300}
 accelerometer_data = {'x_axis': 100, 'y_axis': 2033, 'z_axis': 300}
 gyroscope_data     = {'x_axis': 150, 'y_axis': 203, 'z_axis': 400}
@@ -9,15 +9,33 @@ grip_url          = "http://localhost:3000/grip"
 accelerometer_url = "http://localhost:3000/accelerometer"
 gyroscope_url     = "http://localhost:3000/gyroscope"
 
-def post(json_data, url):   
-    requests.post(url, json = json_data)
+def post(data, url):
+    if is_dict(data) == True:
+        json_data = jsonify(data)   
+        requests.post(url, data = json_data)
+    else: print('Error, data not a dictionary!')    
+    
+def is_dict(data):
+    if type(data) == dict:
+        return True
+    return False 
 
 def jsonify(data):
-    if type(data) == dict:
-        return json.dumps(data)
-    return "Error, argument is not a dictionary!"
-
+    return json.dumps(data)
+ 
 if __name__ == '__main__':
-    while True:
-        print('Data')
+    for i in range (0, 3):
+        # Send accelerometer mock data
+        time.sleep(1)
+        post(accelerometer_data, accelerometer_url)
+
+        # Send grip mock data
+        time.sleep(1)
+        post(grip_data, grip_url)
+
+        # Send gyroscope mock data
+        time.sleep(1)
+        post(gyroscope_data, gyroscope_url)
+
+
 
