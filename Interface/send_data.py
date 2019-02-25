60bf96c7ca2b1081a876f3868be9ef5eeeba686a
KacperKubara <kjk1u17@soton.ac.uk>
2019-02-25 12:02:47 +0000

    send_data.py works now !

--- Interface/send_data.py
+++ Interface/send_data.py
@@ -21,13 +21,27 @@ def unit_test():
     print(is_dict(10))
     
     # Testing post():
-    print('\n Testing post()...')
-    post(accelerometer_data, accelerometer_url)
+    print('\nTesting send_data()...')
+    # Send accelerometer_data
+    send_data(accelerometer_url, accelerometer_data)
+    time.sleep(1)
+
+    # Send grip_data
+    send_data(grip_url, grip_data)
+    time.sleep(1)
+    
+    # Send gyroscope_data
+    send_data(gyroscope_url, gyroscope_data)
     time.sleep(1)
     print('Success!')    
 
-def post(dict_data, url):
-    requests.post(url, dict_data)
+def send_data(url, dict_data):
+    if is_dict(dict_data) is True:
+        try:
+            requests.post(url, dict_data)
+        except ValueError:
+            print('Error during post request!')
+    else: print('Data is not a dict!')
 
 def is_dict(dict_data):
     if type(dict_data) == dict:
@@ -35,7 +49,6 @@ def is_dict(dict_data):
     return False 
  
 if __name__ == '__main__':
-    requests.post("http://localhost:3000/accelerometer", data = {'x_axis': 100, 'y_axis': 2033, 'z_axis': 300})
-
+    unit_test()
 
 
