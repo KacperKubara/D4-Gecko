3f96c27004bb015b3f05e8abc7472b271847f4d8
redscorp98 <sm_ja1@live.co.uk>
2019-02-28 19:45:21 +0000

    connected sockets and stuff. program runs from sockets_example.py

--- Interface/d4_conversion_classes.py
+++ Interface/d4_conversion_classes.py
@@ -8,17 +8,23 @@ class Decoder():
         self.accelerometer_url = "http://138.68.140.17/accelerometer"
         self.gyroscope_url     = "http://138.68.140.17/gyroscope"
         self.interrupt = 0
+        self.interrupt_triggered = 0
         self.force_1 = 0
         self.force_2 = 0
+        self.send_data_toggle = True
         self.force_3 = 0
         self.timestamp = 0
         self.queue = []
+        self.final_queue = []
         threading.Thread(target=self.queue_length).start()
 
     def data_manipulation(self, data):
         self.decode_data(data)
         boolean = self.interrupt
         self.queue_data()
+        print('Queue:')
+        print(self.queue)
+        print('Queue length:' + str(len(self.queue)))
     
     def decode_data(self, data):
         array = data.split(',')
@@ -27,6 +33,8 @@ class Decoder():
         self.force_2   = int(array[2])
         self.force_3   = int(array[3])
         self.timestamp = time.time()
+        if (self.interrupt == 1):
+            self.interrupt_triggered = 1
 
     def queue_data(self):
         dictionary = {'front_grip': self.force_1, 'rear_grip': self.force_2,
@@ -46,7 +54,7 @@ class Decoder():
             
     def send_data(self):
         formatted_data = {}
-        for data in self.queue:
+        for data in self.final_queue:
             formatted_data['front_grip']  = data['front_grip']
             formatted_data['rear_grip']   = data['rear_grip']
             formatted_data['bottom_grip'] = data['bottom_grip']
@@ -55,67 +63,66 @@ class Decoder():
     def queue_length(self):
         while True:
             # print('queue_length(): '+ str(self.interrupt))
-            while self.interrupt == 0:
-                print('interrupt == 0')
+            while self.interrupt_triggered == 0:
+                #print(self.queue)
                 time.sleep(0.2)
                 if len(self.queue) >= 2:
+                    #print('interrupt == 0')
                     length = len(self.queue)
+                    #print(length)
                     front_time = (self.queue[length-1])['timestamp']
                     back_time = (self.queue[0])['timestamp']
                     time_difference = (front_time - back_time)
-                    print('time difference: ' + str(time_difference))
+                    #print('time difference: ' + str(time_difference))
                     if (time_difference > 2.5):
                         self.dequeue_data()
-            while self.interrupt == 1:
+            while self.interrupt_triggered == 1:
                if len(self.queue) >= 2: 
                     time.sleep(0.2)               
-                    print('interrupt == 1')
+                    #print('interrupt == 1')
                     length = len(self.queue)
                     front_time = (self.queue[length-1])['timestamp']
                     back_time = (self.queue[0])['timestamp']
                     time_difference = (front_time - back_time)
-                    print('time difference: ' + str(time_difference))
+                    #print('time difference: ' + str(time_difference))
                     if(time_difference > 3):
+                        self.send_data_toggle = False
                         print('SENDING DATA...')
-                        print(self.queue)
+                        print(len(self.queue))
+                        self.final_queue = self.queue
+                        self.queue = []
                         self.send_data()
                         print('DATA HAS BEEN SENT!')
+                        self.interrupt_triggered = False 
 
-                        self.interrupt = False 
-                        self.queue = [] # Empty the queue
-
-
+    def start_run(self):
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('0,234,765,667')
+        time.sleep(0.5)
+        self.data_manipulation('1,234,765,667')
+        time.sleep(0.5)
+        while self.send_data_toggle == True:
+            self.data_manipulation('0,284,965,167')
+            time.sleep(0.5)    
+        
+    print('data sent')
 
 if __name__ == '__main__':
     inst = Decoder()
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('1,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('1,234,765,667')
-    time.sleep(0.5)    
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
-    time.sleep(0.5)
-    inst.data_manipulation('0,234,765,667')
+  
+    
--- Interface/sockets_example.py
+++ Interface/sockets_example.py
@@ -1,10 +1,12 @@
 import socketio 
+from d4_conversion_classes import Decoder
 url = 'http://138.68.140.17/' # For the deployed server it will differ
 sio = socketio.Client()
 
 @sio.on('connect')
 def on_connect():
     print('I\'m connected!')
+    inst.start_run()
 
 @sio.on('disconnect')
 def on_disconnect():
@@ -22,6 +24,10 @@ def on_stop(data):
 @sio.on('modify')
 def on_modify(data):
     print('modify received:' + str(data))
+    return('TEST')
 
+
+inst = Decoder()
 sio.connect(url)
 sio.wait()
+
