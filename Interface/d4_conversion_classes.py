5e1a8c2d9ff0a41b673385c182a2a88a3cf674b9
KacperKubara <kjk1u17@soton.ac.uk>
2019-02-28 17:59:28 +0000

    bug fixed

--- Interface/d4_conversion_classes.py
+++ Interface/d4_conversion_classes.py
@@ -1,7 +1,8 @@
 import time
 import threading
 
-class decode_data():
+
+class Decoder():
     def __init__(self):
         self.interrupt = 0
         self.force_1 = 0
@@ -9,21 +10,21 @@ class decode_data():
         self.force_3 = 0
         self.timestamp = 0
         self.queue = []
-        self.t0 = time.time()
-        threading.Thread(target=self.queue_length()).start()
+        threading.Thread(target=self.queue_length).start()
 
-    def decode_data(self,data):
+    def decode_data(self, data):
         array = data.split(',')
-        print(array)
-        self.interrupt = array[0]
-        self.force_1 = array[1]
-        self.force_2 = array[2]
-        self.force_3 = array[3]
-        self.timestamp = time.time() - self.t0
+        self.interrupt = int(array[0])
+        self.force_1   = int(array[1])
+        self.force_2   = int(array[2])
+        self.force_3   = int(array[3])
+        self.timestamp = time.time()
 
     def queue_data(self):
-        dictionary = {'front grip':self.force_1,'rear grip':self.force_2,'bottom grip':self.force_3,'timestamp':self.timestamp}
+        dictionary = {'front grip': self.force_1, 'rear grip': self.force_2,
+                      'bottom grip': self.force_3, 'timestamp': self.timestamp}
         (self.queue).append(dictionary)
+        #print(dictionary)
 
     def dequeue_data(self):
         popped = (self.queue).pop(0)
@@ -35,39 +36,57 @@ class decode_data():
         else:
             return True
 
-    def interrupt_triggered(self):
-        if self.interrupt == 0:
-            return False
-        else:
-            return True
-            
     def queue_length(self):
         while True:
-            while not self.interrupt_triggered():
-                if len(self.queue)!=0:
+            # print('queue_length(): '+ str(self.interrupt))
+            while self.interrupt == 0:
+                print('interrupt == 0')
+                if len(self.queue) >= 2:
                     length = len(self.queue)
                     front_time = (self.queue[0])['timestamp']
                     back_time = (self.queue[length-1])['timestamp']
                     time_difference = (front_time - back_time)
-                    if (time_difference >2.5):
+                    if (time_difference > 2.5):
                         self.dequeue_data()
-            while self.interrupt_triggered():
-                length = len(self.queue)
-                front_time = (self.queue[0])['timestamp']
-                back_time = (self.queue[length-1])['timestamp']
-                time_difference = (front_time - back_time)
-                if(time_difference >3):
-                    print(self.queue)
-                    self.interrupt_triggered = False
-        
-    
-    def data_manipulation(self,data):
-        self.decode_data(data)
-        boolean = self.interrupt_triggered()
-        self.queue_data()
-        
-if __name__ == '__main__':
-    inst = decode_data()
-    inst.data_manipulation('1,234,765,667')
-    
+            while self.interrupt == 1:
+               if len(self.queue) >= 2:                
+                    print('interrupt == 1')
+                    length = len(self.queue)
+                    front_time = (self.queue[0])['timestamp']
+                    back_time = (self.queue[length-1])['timestamp']
+                    time_difference = (front_time - back_time)
+                    if(time_difference > 3):
+                        print(self.queue)
+                        self.interrupt = False
 
+    def data_manipulation(self, data):
+        self.decode_data(data)
+        boolean = self.interrupt
+        self.queue_data()
+
+
+if __name__ == '__main__':
+    inst = Decoder()
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('1,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
+    time.sleep(0.5)
+    inst.data_manipulation('0,234,765,667')
